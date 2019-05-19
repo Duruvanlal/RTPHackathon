65497aa5cead1b8baff704b7d51789930ffca182
saurabh@swapstech.com <saurabh@swapstech.com>
2019-05-18 23:01:48 -0500

    Uploaded

--- Employee-App/employee-ui/src/app/pages/new-Invoice/new.invoice.component.ts
+++ Employee-App/employee-ui/src/app/pages/new-Invoice/new.invoice.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { GlobalProperty } from '../../../global';
+import { UserService } from '../../services/user.service';
+import { Invoice } from '../../models/invoice.mode';
+
 
 @Component({
   selector: 'app-new-invoice',
   templateUrl: './new.invoice.component.html',
   styleUrls: ['./new.invoice.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers:[UserService]
 })
 export class NewInvoiceComponent implements OnInit {
+  public loading = true;
+  public user:any;
+  public invoice = new Invoice();
+  public userList = [];
 
-  constructor() { }
 
-  ngOnInit() {  }
+
+
+  constructor(private global:GlobalProperty,
+            private userService:UserService) { }
+
+  ngOnInit() { 
+    if(this.global.principal){
+      this.user = JSON.parse(this.global.principal);
+      console.log("this.user",this.user);
+      this.getAllUsersInfo();
+    }
+  }
+
+  // Load all Users Information.
+  private getAllUsersInfo(){
+    this.userService.getAllUsers().subscribe((response)=>{
+        console.log("Response = " , response);
+        this.userList = <any>response;
+        this.loading = false;
+    });
+  }
+
+  // Update Buyer Information on dropdown change.
+  public selectSender(event){
+    console.log(event);
+    this.invoice.sender = event.value.company;
+    console.clear();
+    console.log("this.invoice.sender = ", this.invoice.sender);
+  }
+
+  public selectReceiver(event){
+    console.log(event);
+    this.invoice.receiver = event.value.company;
+    console.clear();
+    console.log("this.invoice.sender = ", this.invoice.sender);
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
