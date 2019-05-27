083dd2d00abd8173faa32d222efe18c7c50807e2
unknown <ganesh.m@swapstech.com>
2019-05-27 10:33:53 +0530

    Hackathon project updated

--- Hackathon-App/hackathon-ui/src/global.ts
+++ Hackathon-App/hackathon-ui/src/global.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalProperty {
-    url = 'http://localhost:9090/hackathon/';
+    url = 'http://ec2-3-17-234-174.us-east-2.compute.amazonaws.com:9090/hackathon/';
     yodleeUrl = 'https://sandbox.api.yodlee.com/ysl';
     principal = localStorage.getItem("user");
     yoddleToken = localStorage.getItem("yoddleToken");
